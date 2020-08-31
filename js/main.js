4a315a64024b97a2b4bb5d2f079d05fe129753ca
Lukega13 <68627544+Lukega13@users.noreply.github.com>
2020-08-31 17:04:45 -0300

    Carousel changes

--- js/main.js
+++ js/main.js
@@ -6,38 +6,72 @@ window.addEventListener("scroll", function () {
 
 
 //Carousel
-let liEls = document.querySelectorAll('#carousel-item');
-let index = 0;
+window.onload = mainTimer()
+var intervalo = setInterval(mainTimer, 30000)
 
-intervalo = setInterval(timer, 20000)
-window.onload = intervalo
+function mainTimer() {
+    setTimeout(timer, 0)
+    setTimeout(timer2, 10000)
+    setTimeout(timer3, 20000)
+}
 
 function timer() {
-    show(+1)
+    showSlide(1)
+}
+function timer2() {
+    showSlide(2)
+}
+function timer3() {
+    showSlide(3)
 }
 
 
-function show(increase) {
-    index = index + increase;
+function showSlide(number) {
+    var carousel1 = document.getElementById('carousel-image-1')
+    var carousel2 = document.getElementById('carousel-image-2')
+    var carousel3 = document.getElementById('carousel-image-3')
 
-    if (index == '-1') {
-        index = 2
-        liEls[index].scrollIntoView({ behavior: 'smooth' });
-    } else if (index == '3') {
-        index = 0
-        liEls[index].scrollIntoView({ behavior: 'smooth' });
-    } else {
-        let liEls = document.querySelectorAll('#carousel-item');
-        liEls[index].scrollIntoView({ behavior: 'smooth' });
+
+    if (number == 1) {
+        document.getElementById('carousel-button-1').classList.add('slide-button-active')
+        document.getElementById('carousel-button-2').classList.remove('slide-button-active')
+        document.getElementById('carousel-button-3').classList.remove('slide-button-active')
+
+        carousel1.style.opacity = '1'
+        carousel2.style.opacity = '0'
+        carousel3.style.opacity = '0'
+
+        carousel1.style.transition = '3s'
+        carousel2.style.transition = '3s'
+        carousel3.style.transition = '3s'
+
+    } else if (number == 2) {
+        document.getElementById('carousel-button-1').classList.remove('slide-button-active')
+        document.getElementById('carousel-button-2').classList.add('slide-button-active')
+        document.getElementById('carousel-button-3').classList.remove('slide-button-active')
+
+        carousel1.style.opacity = '0'
+        carousel2.style.opacity = '1'
+        carousel3.style.opacity = '0'
+
+        carousel1.style.transition = '3s'
+        carousel2.style.transition = '3s'
+        carousel3.style.transition = '3s'
+
+    } else if (number == 3) {
+        document.getElementById('carousel-button-1').classList.remove('slide-button-active')
+        document.getElementById('carousel-button-2').classList.remove('slide-button-active')
+        document.getElementById('carousel-button-3').classList.add('slide-button-active')
+
+        carousel1.style.opacity = '0'
+        carousel2.style.opacity = '0'
+        carousel3.style.opacity = '1'
+
+        carousel1.style.transition = '3s'
+        carousel2.style.transition = '3s'
+        carousel3.style.transition = '3s'
     }
+
 }
 
-function setSlide(number) {
-    index = number
-
-    let liEls = document.querySelectorAll('#carousel-item');
-    liEls[index].scrollIntoView({ behavior: 'smooth' });
-}
-
-//ARRUMAR ESSE CAROUSEL PRA QUE ELE NÃO PARE DE MEXER QUANDO USE O SCROLL DO MOUSE
-//FAZER OUTRO CAROUSEL MAS QUE ELE TROQUE EM FADE PRA VER QUAL FICA MELHOR
+//REINICIAR ESSE TIMER QUANDO ELE CLICA EM UM BOTAO
